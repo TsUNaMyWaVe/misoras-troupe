e04e48639689e93eb30bd1ae186db4cce0cde0cf
TsUNaMyWaVe <souseisekiftw@gmail.com>
2023-12-08 12:46:35 +0200

    Better resize values

--- views/resizeScript.js
+++ views/resizeScript.js
@@ -3,7 +3,7 @@ document.querySelector('#dynamicTop').style.top = `${start_size}px`;
 addEventListener('resize', ()=>{
    let prop = start_size;
    if (window.innerWidth/screen.width < 0.877) {
-       prop = 400;
+      prop = 550;
 }
    document.querySelector('#dynamicTop').style.top = `${prop}px`;
 });
